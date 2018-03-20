50c9603634a4cdfeb871eb72b2ad157063862962
Matias Harju <matias.harju@aalto.fi>
2018-03-20 23:46:38 +0100

    Add static audio source

--- Assets/360VideoStuff/FreudAVSync.cs
+++ Assets/360VideoStuff/FreudAVSync.cs
@@ -12,10 +12,14 @@ private bool intermusicTriggered = false;
 public float FreudAudioStarts = 0.00F;	// Sound events' starting times in seconds
 public float InteractiveMusicStarts = 12.00F;
 
+private GameObject staticAudioSource;
+
 VideoPlayer videoPlayer;
 
     void Start()
     {
+		staticAudioSource = GameObject.Find("StaticAudioSource");
+
         videoPlayer = GetComponent<VideoPlayer> ();	
 		videoPlayer.Play();
     }
@@ -24,7 +28,7 @@ VideoPlayer videoPlayer;
 	{	
 		if (videoPlayer.time >= (FreudAudioStarts - 0.005) && videoPlayer.time < (FreudAudioStarts + 0.02) && freudTriggered == false)
 		{
-			AkSoundEngine.PostEvent("PlayFreudAudio", gameObject);
+			AkSoundEngine.PostEvent("PlayFreudAudio", staticAudioSource.gameObject);
 			Debug.Log("<color=green>Start Freud Audio at 00:00</color>" + " " + videoPlayer.time);
 			freudTriggered = true;
 		}
@@ -37,7 +41,7 @@ VideoPlayer videoPlayer;
 /* 
 		if (videoPlayer.time >= (InteractiveMusicStarts - 0.005) && videoPlayer.time < (InteractiveMusicStarts + 0.02) && intermusicTriggered == false)
 		{
-			AkSoundEngine.PostEvent("Interactive_Music_2", gameObject);
+			AkSoundEngine.PostEvent("Interactive_Music_2", Sphere.gameObject);
 			Debug.Log("<color=blue>Start Interactive Music at 12:00</color>" + " " + videoPlayer.time);
 			intermusicTriggered = true;
 		}
